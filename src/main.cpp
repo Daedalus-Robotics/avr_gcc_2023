be33debf9432045cc6cf92468522069110368ad1
literallyanything <hunterbaker@me.com>
2023-10-02 20:30:00 -0400

    Add micro-ros stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <Arduino.h>
-void setup() {
-// write your initialization code here
+#include <micro_ros_platformio.h>
+
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#define EXECUTOR_HANDLES 0
+
+rcl_allocator_t allocator;
+rclc_support_t support;
+rclc_executor_t executor;
+
+void setup()
+{
+    Serial.begin(115200);
+    set_microros_serial_transports(Serial);
+
+    allocator = rcl_get_default_allocator();
+    rclc_support_init(&support, 0, nullptr, &allocator);
+    rclc_executor_init(&executor, &support.context, EXECUTOR_HANDLES, &allocator);
 }
 
-void loop() {
-// write your code here
+void loop()
+{
+    rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10));
 }
