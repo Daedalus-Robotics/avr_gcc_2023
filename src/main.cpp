6c75f059d075b6c8d248ed9180cdcf3c5e7131a0
literallyanything <hunterbaker@me.com>
2023-10-21 20:03:59 -0400

    Add setup and cleanup for system and gimbal nodes

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,39 @@ rcl_allocator_t allocator;
 rclc_support_t support;
 rclc_executor_t executor;
 
-void setup()
+GimbalMotorNode *xAxisMotorNode;
+//GimbalMotorNode *yAxisMotorNode;
+
+FLASHMEM void cleanup()
+{
+//    yAxisMotorNode->cleanup();
+    xAxisMotorNode->cleanup();
+    cleanupSystem();
+}
+
+FASTRUN void eStop()
+{
+    xAxisMotorNode->eStop();
+//    yAxisMotorNode->eStop();
+}
+
+FLASHMEM void setup()
 {
     Serial.begin(115200);
     set_microros_serial_transports(Serial);
 
+    initSystem(cleanup, eStop);
+
+    xAxisMotorNode = new GimbalMotorNode("gimbal/x", 1);
+//    yAxisMotorNode = new GimbalMotorNode("gimbal/y", 1);
+
     allocator = rcl_get_default_allocator();
-    rclc_support_init(&support, 0, nullptr, &allocator);
-    rclc_executor_init(&executor, &support.context, EXECUTOR_HANDLES, &allocator);
+    HANDLE_ROS_ERROR(rclc_support_init(&support, 0, nullptr, &allocator), true);
+    HANDLE_ROS_ERROR(rclc_executor_init(&executor, &support.context, EXECUTOR_HANDLES, &allocator), true);
+
+    setupSystem(&support, &executor);
+    xAxisMotorNode->setup(&support, &executor);
+//    yAxisMotorNode->setup(&support, &executor);
 }
 
 void loop()
